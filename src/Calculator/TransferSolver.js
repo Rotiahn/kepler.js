1826c71f996bcf55fdce6655a8d574e358c686b2
Rotiahn <mail@rotiahn.com>
2016-03-15 23:46:55 -0400

    - Beginning of KEPLER.TransferSolver.minDeltaV().   Needs revision because we're using 1 second precision and it needs better than straight iterative tester.

--- src/Calculator/TransferSolver.js
+++ src/Calculator/TransferSolver.js
@@ -8,10 +8,49 @@ KEPLER.TransferSolver = {};
  * @author Rotiahn / https://github.com/Rotiahn/
  * @param {KEPLER.Orbit} orbit1 - The initial orbit
  * @param {KEPLER.Orbit} orbit2 - The destination orbit (mAnomaly should be set to value corresponding to DEPARTURE time)
- * @param {number} maxTime - the maximum amount of time (s) before the transfer should finish (time waiting + transfer duration)
  * @module kepler
  */
-KEPLER.TransferSolver.minDeltaV = function (orbit1, orbit2, maxTime) {
+KEPLER.TransferSolver.minDeltaV = function (orbit1, orbit2) {
+    //Given a starting orbit, ending orbit and a maximum,start object,end object and start time, find the transfer orbit which
+    // arrives at the target object with the least amount of delta_v
+    // Allows delays in departure
+    var object1 = orbit1.getElements();
+    var object2 = orbit2.getElements();
+
+    var departTimeMin = 0;
+    var departTimeMax = Math.min(
+                             object1.T  //either full orbit of departure object
+                            ,object2.T  //or the full orbit of target object
+                            );
+    var travelTimeMin = 1;
+    var travelTimeMax = Math.max(
+                             object1.T  //either full orbit of departure object
+                            ,object2.T  //or the full orbit of target object
+                            );
+
+
+	var joptions=[];
+	for (var j=departTimeMin; j<=departTimeMax; j++) {
+        options=[];
+        for (var i=travelTimeMin; i<=travelTimeMax; i++) {
+            var time1=j;
+            var time2=j+i;
+            var transfer = new KEPLER.Transfer(orbit1,orbit1,i);
+            options.push(transfer);
+            //console.log(j,i,transfer.delta_v)
+        }
+        var best = options[0];
+        for (var i=0;i < options.length;i++) {
+            if (options[i].delta_v <= best.delta_v) {best = options[i];};
+        }
+        joptions.push(best);
+    }
+    var jbest = joptions[0];
+    for (var j=0;j<joptions.length;j++) {
+        if (joptions[j].delta_v <= jbest.delta_v) {jbest = joptions[j];};
+    }
+    return jbest;
+
 
 }
 
