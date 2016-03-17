982db7a46f6fe55ba650e9ffe7eb3de3641cc663
Rotiahn <mail@rotiahn.com>
2016-03-17 12:25:49 -0400

    - Exploring ideas for Transfer Solver MinDeltaV

--- src/Calculator/TransferSolver.js
+++ src/Calculator/TransferSolver.js
@@ -29,9 +29,75 @@ KEPLER.TransferSolver.minDeltaV = function (orbit1, orbit2) {
                             );
 
 //Need a mechanism for optimizing non-coplanar comparisons.
-// Idea1 - Break into 10 chunks, find best, then 10 chunks, etc.
-// Idea2 - Binary solver
+// Idea1 -  Chunks + newton solver:
+//          1. Break into X chunks, where X = (larger Period / smaller period) = travelTimeMax / departTimeMax
+//          2. Find chunk with lowest deltaV expectation
+//          3. Define chunkMin & chunkMax
+//          4. Test point = (chunkMin + chunkMax) /2
+//          5. Find deltaV at Test Point (deltaV0)
+//          6. Find deltaV at test point+1s (deltaV1)
+//          7. Determine deltaV slope (deltaV1-deltaV0)
+//          8. If deltaV slope >0, chunkMax = Testpoint, goto 4
+//          9. If deltaV slope <0, chunkMin = Testpoint, goto 4
+//          10.If deltaV slope ~=0, Found minimum deltaV!
+//
+// Idea2 - Global Binary Tree solver
 // Idea3 - Modified Lambert Solver?
+
+	    //deltaV = v_initial.length() + v_final.length()
+	    //v_initial = (r_final - f*r_initial)/g
+	    //d(v_initial)/dz = (r_final - df/dz * r_initial) / dg/dz
+
+	    //f = 1 - y/r_initial_length
+	    //y = r_initial + r_final + (A/Math.sqrt(C))*(z*S - 1)
+	    //dy/dz = 0     + 0       + (A/Math.sqrt(C))*(S)
+	    //dy/dz = (A/Math.sqrt(C))*(S)
+        //df/dz = df/dy * dy/dz
+	    //df/dz = -(1/r_initial_length)(dy/dz)
+	    //df/dz = -(1/r_initial_length)((A/Math.sqrt(C))*(S))
+
+        //g = A*Math.sqrt(y/mu) = A*(y^0.5)(mu^-0.5)
+        //dg/dz = dg/dy * dy/dz =  (A/Math.sqrt(mu))*((1/2)*y^-0.5) * (A/Math.sqrt(C))*(S)
+        //dg/dz = (S*A^2 / Math.sqrt(C*mu) ) * (1/Math.sqrt(y))
+
+        //y is at an extrema if A=0, C=0 OR S=0
+        //f is at an extrema if A=0, OR S=0
+        //g is at an extrema if A=0, S=0 OR y=0
+
+	    //v_initial = (r_final - f*r_initial)/g
+	    //v_initial = (r_final - f*r_initial)*(g^-1)
+	    //v_initial =  r_final*(g^-1) - f*r_initial*(g^-1)
+	    //d(v_initial)/dz = d(v_initial)/dg * dg/dz
+	    //d(v_initial)/dz = (-1*r_final*(g^-2) - ( (-1)*f*r_initial*(g^-2) + (df/dg)*r_initial*(g^-1) )) * (dg/dz)
+	    //df/dg = df/dz * dz/dg = df/dz * dz/dy * dy/dg
+
+	    //y = r_initial_length + r_final_length + (A/Math.sqrt(C))*(z*S - 1)
+	    //y-(r_initial_length + r_final_length) = (A/Math.sqrt(C))*(z*S - 1)
+	    // ( y-(r_initial_length + r_final_length) ) / (A/Math.sqrt(C)) = (z*S - 1)
+
+	    // 1+ ( y-(r_initial_length + r_final_length) ) / (A/Math.sqrt(C)) = (z*S)
+	    // z = 1/S + ( y-(r_initial_length + r_final_length) ) / ((S*A)/Math.sqrt(C))
+        // dz/dy = (1/ ((S*A)/Math.sqrt(C))
+
+        //g = A*Math.sqrt(y/mu)
+        //g/A = Math.sqrt(y/mu)
+        // ( g/A )^2 = y/mu
+        // y = (mu*g^2)/A^2
+        //dy/dg = 2*(mu*g)/A^2
+
+        //df/dg = df/dz * dz/dy * dy/dg
+        //df/dg = -(1/r_initial_length)((A/Math.sqrt(C))*(S)) * (1/ ((S*A)/Math.sqrt(C)) * 2*(mu*g)/A^2
+        //df/dg = -(1/r_initial_length)((A/Math.sqrt(C))*(S)) * (Math.sqrt(C)/ ((S*A)) * 2*(mu*g)/A^2
+        //df/dg = (1/(A^2)) * (4) * ( -((mu*g)/r_initial_length) )
+        //df/dg = ((4/(A^2)) * ( -((mu*g)/r_initial_length) ))
+
+	    //d(v_initial)/dz = (-1*r_final_length*(g^-2) - ( (-1)*f*r_initial_length*(g^-2) + ( (4/(A^2))*(-((mu*g)/r_initial_length)) )*r_initial_length*(g^-1) )) * (dg/dz)
+	    //d(v_initial)/dz = (-1*r_final_length*(g^-2) - ( (-1)*f*r_initial_length*(g^-2) + ( (4/(A^2))*(-(mu*g)) )*(g^-1) )) * (dg/dz)
+	    //d(v_initial)/dz = (-1*r_final_length*(g^-2) + ( f*r_initial_length*(g^-2) + ( (-4/(A^2))*(-(mu)) ) )) * (dg/dz)
+	    //d(v_initial)/dz = (-1*r_final_length*(g^-2) + ( (1 - y/r_initial_length)*r_initial_length*(g^-2) + ( (-4/(A^2))*(-(mu)) ) )) * (dg/dz)
+	    //d(v_initial)/dz = (-1*r_final_length*(g^-2) + ( (r_initial_length*(g^-2) - r_initial_length*(g^-2)*(y/r_initial_length)) + ( (-4/(A^2))*(-(mu)) ) )) * (dg/dz)
+	    //d(v_initial)/dz = (-1*r_final_length*(g^-2) + (r_initial_length*(g^-2) - (g^-2)*(y)) + (-4/(A^2))*(-(mu)) ) * (dg/dz)
+	    //d(v_initial)/dz = ( (r_initial_length - r_final_length)*(g^-2) - (g^-2)*(y)) + (-4/(A^2))*(-(mu)) ) * (dg/dz)
 
 //There will be local minima (based on behavior of smaller orbit), but best minima will be best local minimum for particular position of larger orbit.
 
