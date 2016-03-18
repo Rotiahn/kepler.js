244c3ebebb7bdc9a890f6c9b5991035eef57da67
Rotiahn <mail@rotiahn.com>
2016-03-18 10:00:49 -0400

    - Identify Chunk characteristics

--- src/Calculator/TransferSolver.js
+++ src/Calculator/TransferSolver.js
@@ -14,35 +14,48 @@ KEPLER.TransferSolver.minDeltaV = function (orbit1, orbit2) {
     //Given a starting orbit, ending orbit and a maximum,start object,end object and start time, find the transfer orbit which
     // arrives at the target object with the least amount of delta_v
     // Allows delays in departure
+
+    //Need a mechanism for optimizing non-coplanar comparisons.
+    // Idea1 -  Chunks + newton solver:
+    //          1. Break potential launch times into X chunks, where X = (larger Period / smaller period). Chunk size = smaller period
+    //          2. Find chunk with lowest deltaV expectation
+    //          3. Define chunkMin & chunkMax
+    //          4. Test point = (chunkMin + chunkMax) /2
+    //          5. Find deltaV at Test Point (deltaV0)
+    //          6. Find deltaV at test point+1s (deltaV1)
+    //          7. Determine deltaV slope (deltaV1-deltaV0)
+    //          8. If deltaV slope >0, chunkMax = Testpoint, goto 4
+    //          9. If deltaV slope <0, chunkMin = Testpoint, goto 4
+    //          10.If deltaV slope ~=0, Found minimum deltaV!
+    //
+    // Idea2 - Global Binary Tree solver
+    // Idea3 - Modified Lambert Solver?
+
     var object1 = orbit1.getElements();
     var object2 = orbit2.getElements();
 
-    var departTimeMin = 0;
-    var departTimeMax = Math.min(
+    var periodSmall = Math.min(
                              object1.T  //either full orbit of departure object
                             ,object2.T  //or the full orbit of target object
                             );
-    var travelTimeMin = 1;
-    var travelTimeMax = Math.max(
+    var periodLarge = Math.max(
                              object1.T  //either full orbit of departure object
                             ,object2.T  //or the full orbit of target object
                             );
 
-//Need a mechanism for optimizing non-coplanar comparisons.
-// Idea1 -  Chunks + newton solver:
-//          1. Break into X chunks, where X = (larger Period / smaller period) = travelTimeMax / departTimeMax
-//          2. Find chunk with lowest deltaV expectation
-//          3. Define chunkMin & chunkMax
-//          4. Test point = (chunkMin + chunkMax) /2
-//          5. Find deltaV at Test Point (deltaV0)
-//          6. Find deltaV at test point+1s (deltaV1)
-//          7. Determine deltaV slope (deltaV1-deltaV0)
-//          8. If deltaV slope >0, chunkMax = Testpoint, goto 4
-//          9. If deltaV slope <0, chunkMin = Testpoint, goto 4
-//          10.If deltaV slope ~=0, Found minimum deltaV!
-//
-// Idea2 - Global Binary Tree solver
-// Idea3 - Modified Lambert Solver?
+
+    var chunkSize = periodSmall;
+    var chunkCount = Math.ceil(periodLarge / chunkSize);  // We will include the entirety of last chunk, even if that chunk extends past periodLarge
+
+
+    var departTimeMin = 0;
+    var departTimeMax = chunkSize * chunkCount;
+    var travelTimeMin = 1;
+    var travelTimeMax = periodLarge;
+
+
+    //Cycle through each Chunk and determine its approximate minima to identify chunk with preferred
+
 
 
 
