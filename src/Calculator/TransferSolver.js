4aa8acd855d0d8b82287c3ca237d21e12af04f26
Rotiahn <mail@rotiahn.com>
2016-03-20 00:00:46 -0400

    - Added KEPLER.TransferSolver.minDeltaV_LaunchNow() to calculate minDeltaV solution for an immediate launch scenario.

--- src/Calculator/TransferSolver.js
+++ src/Calculator/TransferSolver.js
@@ -4,6 +4,82 @@
  */
 KEPLER.TransferSolver = {};
 
+/** A function for calculating the transfer with minimum deltaV assuming launch immediately
+ * @author Rotiahn / https://github.com/Rotiahn/
+ * @param {KEPLER.Orbit} orbit1 - The initial orbit
+ * @param {KEPLER.Orbit} orbit2 - The destination orbit (mAnomaly should be set to value corresponding to DEPARTURE time)
+ * @returns {KEPLER.Transfer} transfer - The optimum transfer orbit to minimize deltaV launching now.
+ * @module kepler
+ */
+KEPLER.TransferSolver.minDeltaV_LaunchNow = function (orbit1, orbit2) {
+    var object1 = orbit1.clone();
+    var object2 = orbit2.clone();
+
+    var object1Elements = object1.getElements();
+    var object2Elements = object2.getElements();
+
+    var periodSmall = Math.min(
+                             object1Elements.T  //either full orbit of departure object
+                            ,object2Elements.T  //or the full orbit of target object
+                            );
+    var periodLarge = Math.max(
+                             object1Elements.T  //either full orbit of departure object
+                            ,object2Elements.T  //or the full orbit of target object
+                            );
+
+    var travelTimeMin = 1;
+    var travelTimeMax = periodLarge;
+
+    var testTime1 = Math.ceil( (travelTimeMin + travelTimeMax)/2 );
+    var testVectors1 = KEPLER.Lambert(object1,object2,testTime1);
+    var deltaV1 = testVectors1.departure.length() + testVectors1.arrival.length();
+
+    var testTime2 = testTime1+1;
+    var testVectors2 = KEPLER.Lambert(object1,object2,testTime2);
+    var deltaV2 = testVectors2.departure.length() + testVectors2.arrival.length();
+
+    var deltaVSlope = (deltaV2-deltaV1)/1; //When deltaVSlope = 0, we have reached an extrema.
+
+    //console.log(testTime1,testTime2,deltaV1,deltaV2,deltaVSlope);
+
+    var i = 0;
+
+    while ( travelTimeMax-travelTimeMin > 1 ) {
+
+        if (deltaVSlope > 0) {
+            //deltaV is increasing, set travelTimeMax to be testTime1 (minima must be before this time)
+            travelTimeMax = testTime1;
+        } else if (deltaVSlope < 0) {
+            //deltaV is decreasing, set travelTimeMin to be testTime1 (minima must be after this time)
+            travelTimeMin = testTime1;
+        } else {
+            //deltaV is 0, set travelTimeMin to be testTime1 AND set travelTimeMax to be testTime2  (we stumbled upon the minima exactly)
+
+        }
+
+        testTime1 = Math.ceil( (travelTimeMin + travelTimeMax)/2 );
+        testVectors1 = KEPLER.Lambert(object1,object2,testTime1);
+        deltaV1 = testVectors1.departure.length() + testVectors1.arrival.length();
+
+        testTime2 = testTime1+1;
+        testVectors2 = KEPLER.Lambert(object1,object2,testTime2);
+        deltaV2 = testVectors2.departure.length() + testVectors2.arrival.length();
+
+        deltaVSlope = (deltaV2-deltaV1)/1;
+
+        //console.log(i,testTime1,travelTimeMin,travelTimeMax,deltaV1,deltaV2,deltaVSlope);
+
+        i++;
+        if (i>100) {throw 'KEPLER.TransferSolver.minDeltaV_LaunchNow took too long with to calculate transfer';};
+
+    }
+    //Found Optimum Travel time: testTime1
+
+    var optimumTransfer = new KEPLER.Transfer(orbit1,orbit2,testTime1);
+    return optimumTransfer;
+}
+
+
 /** A function for calculating the optimum transfer, optimizing for minimum deltaV expenditure
  * @author Rotiahn / https://github.com/Rotiahn/
  * @param {KEPLER.Orbit} orbit1 - The initial orbit
