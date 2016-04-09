b1cdb41203b38b5408e3de9a28bbaaef35de10ff
Rotiahn <mail@rotiahn.com>
2016-04-09 13:42:56 -0400

    - Added a kepler_use_THREEJS.js file

--- utils/build/build_all.sh
+++ utils/build/build_all.sh
@@ -4,53 +4,7 @@
 export BASEDIR=$(cd "$PWD/../../" && pwd) 
 export SRCDIR="$BASEDIR/src"
 export EXTERNALDIR="$BASEDIR/src/external"
+export BUILDSCRIPTS="$BASEDIR/utils/build"
 
-
-#FILE LIST
-FILELIST=(
-    "$SRCDIR/Kepler.js"                         #Base KEPLER file
-
-    "$EXTERNALDIR/three.js/Three.js"            #THREE.JS
-    "$EXTERNALDIR/three.js/math/Matrix4.js"     #THREE.JS
-    "$EXTERNALDIR/three.js/math/Vector3.js"     #THREE.JS
-    "$EXTERNALDIR/external.js"                  #external renaming to KEPLER
-
-    "$SRCDIR/AstroBody.js"                      #Astrobody
-    "$SRCDIR/Orbit.js"                          #Orbit
-    "$SRCDIR/Spacecraft.js"                     #Spacecraft
-    "$SRCDIR/Transfer.js"                       #Transfer
-
-    "$SRCDIR/Calculator/Lambert.js"             #Lambert Solver
-    "$SRCDIR/Calculator/TransferSolver.js"      #Various Transfer Solvers
-
-    "$SRCDIR/examples.js"                       
-)
-
-#count=0
-#while [ "x${FILELIST[count]}" != "x" ]
-#do
-#    count=$(( $count + 1 ))
-#done
-#echo $count
-
-
-#Build Non-Minimized version:
-
-echo "" > "$BASEDIR/kepler.js"
-count=0
-while [ "x${FILELIST[count]}" != "x" ]
-do
-    echo ""                                 >> "$BASEDIR/kepler.js"
-    echo "//File: "${FILELIST[count]}""     >> "$BASEDIR/kepler.js"
-    echo ""                                 >> "$BASEDIR/kepler.js"
-    cat "${FILELIST[count]}"                >> "$BASEDIR/kepler.js"
-
-    count=$(( $count + 1 ))
-done
-echo $count
-
-#Build Minimized 
-
-java -jar "$BASEDIR/utils/build/Compiler/compiler.jar" "$BASEDIR/kepler.js" \
---jscomp_off=globalThis --jscomp_off=checkTypes \
---js_output_file "$BASEDIR/kepler.min.js"
+source "$BUILDSCRIPTS/build_main.sh"
+source "$BUILDSCRIPTS/build_use_THREEJS.sh"
