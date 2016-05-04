f0add5ce80708f2c6974756db8afce1851c21c7a
Rotiahn <mail@rotiahn.com>
2016-05-04 09:42:56 -0400

    - Initial sliders

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,23 @@
-console.log('This would be the main JS file.');
+//console.log('This would be the main JS file.');
+
+$( document ).ready(function() {
+    //Create scene and camera
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( 690, 690 );
+
+    var demoBox = $('#demo');
+    var demoTabs = $('#tabs');
+    demoTabs.tabs();
+
+    $("#slider-star-a").slider();
+    $("#slider-star-ecc").slider();
+    $("#slider-star-mAnomaly").slider();
+    $("#slider-star-rotI").slider();
+    $("#slider-star-rotW").slider();
+    $("#slider-star-rotOmeg").slider();
+
+    $('#demo').append( renderer.domElement );
+});
