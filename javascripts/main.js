9f63e881c0754dc6bead12e4c0a83ab45b4d5bfd
Rotiahn <mail@rotiahn.com>
2016-05-08 01:46:33 -0400

    - Moving sliders now works correctly.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,12 @@
 //console.log('This would be the main JS file.');
 
-$( document ).ready(function() {
+elementChange = function() {
+
+    $('canvas').remove();
+    try {clearInterval(interval);} catch(err) {/*do nothing*/};
     //Create scene and camera
     scene = new THREE.Scene();
+
     camera = new THREE.PerspectiveCamera( 75, 690 / 690, 0.001, 100 );
 	light = new THREE.PointLight(0xFFFFDD);
 	amblight = new THREE.AmbientLight(0x444444);
@@ -13,134 +17,7 @@ $( document ).ready(function() {
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize( 690, 690 );
 
-    var demoBox = $('#demo');
-    var demoTabs = $('#tabs');
-    demoTabs.tabs({
-        collapsible:true
-    });
-
-    $('#slider-star-mass').slider({
-         value:1
-        ,min:0.0
-        ,max:10.1
-        ,step:0.1
-        ,slide: function(event,ui) {
-            $(this).parent().find("input").val($(this).slider("value") + " Sols");
-        }
-    });
-    $('#slider-planet-mass').slider({
-         value:1
-        ,min:0.0
-        ,max:10.1
-        ,step:0.1
-        ,slide: function(event,ui) {
-            $(this).parent().find("input").val($(this).slider("value") + " Earths");
-        }
-    });
-    $('#slider-moon-mass').slider({
-         value:1
-        ,min:0.0
-        ,max:10.1
-        ,step:0.1
-        ,slide: function(event,ui) {
-            $(this).parent().find("input").val($(this).slider("value") + " Moons");
-        }
-    });
-    $('#star-mass').val( function() {
-        return ($('#slider-star-mass').slider("value")  + " Sols");
-    });
-    $('#planet-mass').val( function() {
-        return ($('#slider-planet-mass').slider("value")  + " Earths");
-    });
-    $('#moon-mass').val( function() {
-        return ($('#slider-moon-mass').slider("value")  + " Lunas");
-    });
-    $('#slider-planet-a').slider({
-         value:1
-        ,min:0.0
-        ,max:10.1
-        ,step:0.1
-        ,slide: function(event,ui) {
-            $(this).parent().find("input").val($(this).slider("value") + " AU");
-        }
-    });
-    $('#slider-moon-a').slider({
-         value:30000000
-        ,min: 0.0
-        ,max: 3030000
-        ,step:30000
-        ,slide: function(event,ui) {
-            $(this).parent().find("input").val($(this).slider("value") + " km");
-        }
-    });
-    $('#planet-a').val( function() {
-        return ($('#slider-planet-a').slider("value")  + " AU");
-    });
-    $('#moon-a').val( function() {
-        return ($('#slider-moon-a').slider("value")  + " km");
-    });
-    $("[id^=slider][id$=ecc]").slider({
-         value:0.0
-        ,min:0.0
-        ,max:1.1
-        ,step:0.1
-        ,slide: function(event,ui) {
-            $(this).parent().find("input").val($(this).slider("value") + "");
-        }
-    });
-    $('input[id$=ecc]').val( function() {
-        return ($(this).parent().find("[id^=slider][id$=ecc]").slider("value")  + "");
-    });
-    $("[id^=slider][id$=mAnomaly]").slider({
-         value:0
-        ,min:0.0
-        ,max:360
-        ,step:5
-        ,slide: function(event,ui) {
-            $(this).parent().find("input").val($(this).slider("value") + " \xB0");
-        }
-    });
-    $('input[id$=mAnomaly]').val( function() {
-        return ($(this).parent().find("[id^=slider][id$=mAnomaly]").slider("value")  + " \xB0");
-    });
-    $("[id^=slider][id$=rotI]").slider({
-         value:0
-        ,min:-90
-        ,max:90
-        ,step:5
-        ,slide: function(event,ui) {
-            $(this).parent().find("input").val($(this).slider("value") + " \xB0");
-        }
-    });
-    $('input[id$=rotI]').val( function() {
-        return ($(this).parent().find("[id^=slider][id$=rotI]").slider("value")  + " \xB0");
-    });
-    $("[id^=slider][id$=rotW]").slider({
-         value:0
-        ,min:0.0
-        ,max:360
-        ,step:5
-        ,slide: function(event,ui) {
-            $(this).parent().find("input").val($(this).slider("value") + " \xB0");
-        }
-    });
-    $('input[id$=rotW]').val( function() {
-        return ($(this).parent().find("[id^=slider][id$=rotW]").slider("value")  + " \xB0");
-    });
-    $("[id^=slider][id$=rotOmeg]").slider({
-         value:0
-        ,min:0.0
-        ,max:360
-        ,step:5
-        ,slide: function(event,ui) {
-            $(this).parent().find("input").val($(this).slider("value") + "\xB0");
-        }
-    });
-    $('input[id$=rotOmeg]').val( function() {
-        return ($(this).parent().find("[id^=slider][id$=rotOmeg]").slider("value")  + " \xB0");
-    });
-
-    star = new KEPLER.AstroBody(
+     star = new KEPLER.AstroBody(
          'Star'
         ,$("#slider-star-mass").slider("value") * KEPLER.SOL_MASS
     ,new KEPLER.NULL_ORBIT()
@@ -149,25 +26,22 @@ $( document ).ready(function() {
     star.material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
     star.mesh = new THREE.Mesh( star.geometry, star.material);
     scene.add(star.mesh);
-    star.trailGeom = new THREE.Geometry();
-    star.trail = new THREE.Mesh( star.trailGeom, star.material);
-    scene.add(star.trail);
+    //scene.add(star.trail);
     star.update = function(time){
         this.orbit.addTime(time);
-        var pos = this.getPosition().divideScalar(KEPLER.AU);
+        pos = this.getPosition().divideScalar(KEPLER.AU);
         this.mesh.position.set(
              pos.x
             ,pos.y
             ,pos.z
         );
-        this.trailGeom.vertices.unshift(pos);
     }
     star.update(0);
     star.zoom = function() {
         focus = this;
         camera.position.set(
               this.mesh.position.x
-             ,this.mesh.position.y+5
+             ,this.mesh.position.y
              ,this.mesh.position.z+5
         );
         planet.mesh.scale.set(0.4,0.4,0.4);
@@ -183,9 +57,9 @@ $( document ).ready(function() {
             ,$('#slider-planet-a').slider("value") * KEPLER.AU
             ,$('#slider-planet-ecc').slider("value")
             ,$('#slider-planet-mAnomaly').slider("value")
-            ,$('#slider-planet-rotI').slider("value")
-            ,$('#slider-planet-rotW').slider("value")
-            ,$('#slider-planet-rotOmeg').slider("value")
+            ,$('#slider-planet-rotI').slider("value") * KEPLER.DEGREE
+            ,$('#slider-planet-rotW').slider("value") * KEPLER.DEGREE
+            ,$('#slider-planet-rotOmeg').slider("value") * KEPLER.DEGREE
         )
     );
     planet.geometry = new THREE.SphereGeometry(0.1);
@@ -193,22 +67,29 @@ $( document ).ready(function() {
     planet.mesh = new THREE.Mesh( planet.geometry, planet.material);
     planet.mesh.scale.set(0.4,0.4,0.4);
     scene.add(planet.mesh);
+    planet.trailGeom = new THREE.Geometry();
+    var testE;
+    for (var i=0;i<=361;i++) {
+        testE = i*((2*Math.PI)/361);
+        planet.trailGeom.vertices.push(planet.orbit.getPositionAtE(testE).divideScalar(KEPLER.AU));
+    }
+    planet.trail = new THREE.Line( planet.trailGeom);
+    scene.add(planet.trail);
     planet.update = function(time){
-        planet.orbit.addTime(time);
-        var pos = planet.getPosition().divideScalar(KEPLER.AU);
-        planet.mesh.position.set(
+        this.orbit.addTime(time);
+        pos = this.getPosition().divideScalar(KEPLER.AU);
+        this.mesh.position.set(
              pos.x
             ,pos.y
             ,pos.z
         );
-        //this.trailGeom.unshift(pos);
-    }
+   }
     planet.update(0);
     planet.zoom = function() {
         focus = this;
         camera.position.set(
               this.mesh.position.x
-             ,this.mesh.position.y+.3
+             ,this.mesh.position.y
              ,this.mesh.position.z+.3
         );
         planet.mesh.scale.set(0.04,0.04,0.04);
@@ -224,9 +105,9 @@ $( document ).ready(function() {
             ,$('#slider-moon-a').slider("value") * KEPLER.KM
             ,$('#slider-moon-ecc').slider("value")
             ,$('#slider-moon-mAnomaly').slider("value")
-            ,$('#slider-moon-rotI').slider("value")
-            ,$('#slider-moon-rotW').slider("value")
-            ,$('#slider-moon-rotOmeg').slider("value")
+            ,$('#slider-moon-rotI').slider("value") * KEPLER.DEGREE
+            ,$('#slider-moon-rotW').slider("value") * KEPLER.DEGREE
+            ,$('#slider-moon-rotOmeg').slider("value") * KEPLER.DEGREE
         )
     );
     moon.geometry = new THREE.SphereGeometry(0.1);
@@ -234,31 +115,35 @@ $( document ).ready(function() {
     moon.mesh = new THREE.Mesh( moon.geometry, moon.material);
     moon.mesh.scale.set(0.1,0.1,0.1);
     scene.add(moon.mesh);
+    //moon.trailGeom = new THREE.Geometry();
+    //var testE;
+    //for (var i=0;i<=361;i++) {
+    //    testE = i*((2*Math.PI)/361);
+    //    moon.trailGeom.vertices.push(moon.orbit.getPositionAtE(testE).divideScalar(KEPLER.AU));
+    //}
+    //moon.trail = new THREE.Line( moon.trailGeom);
+    //scene.add(moon.trail);
     moon.update = function(time){
         this.orbit.addTime(time);
-        var pos = this.getPosition().divideScalar(KEPLER.AU);
+        pos = this.getPosition().divideScalar(KEPLER.AU);
         this.mesh.position.set(
              pos.x
             ,pos.y
             ,pos.z
         );
-        //this.trailGeom.unshift(pos);
-    }
+     }
     moon.zoom = function() {
         focus = this;
         camera.position.set(
              this.mesh.position.x
-            ,this.mesh.position.y+.05
+            ,this.mesh.position.y
             ,this.mesh.position.z+.05
         );
-        planet.mesh.scale.set(0.04,0.04,0.04);
-        moon.mesh.scale.set(0.01,0.01,0.01);
+        planet.mesh.scale.set(0.01,0.01,0.01);
+        moon.mesh.scale.set(0.002,0.002,0.002);
         camera.lookAt(this.mesh.position);
     }
 
-
-    camera.position.set(0,5,5);
-    camera.lookAt(star.mesh.position);
 
     star.zoom();
 
@@ -281,8 +166,197 @@ $( document ).ready(function() {
         focus.zoom();
         //render();
     }
-    setInterval(update_1d,1000/30);
+    interval = setInterval(update_1d,1000/30);
 
 
     $('#demo>h3').after( renderer.domElement );
+};
+
+
+$( document ).ready(function() {
+    //ADD FUNCTIONS TO KEPLER.Orbit()
+    KEPLER.Orbit.prototype.getPositionAtE = function(E) {
+        //Part I: gather Orbital Elements
+        this.updateAllElements();
+        var elements = this.getElements();
+        var tempMAnomaly = E - (elements.ecc * Math.sin(E));
+
+        //Part II: Create clone
+        //console.log(this);
+        var clone = new KEPLER.Orbit(
+             this.primary
+            ,elements.a
+            ,elements.ecc
+            ,tempMAnomaly //elements.mAnomaly
+            ,elements.rotI
+            ,elements.rotW
+            ,elements.rotOmeg
+        );
+        //console.log(clone,clone.primary);
+        return clone.getPosition();
+    }
+    KEPLER.NULL_ORBIT.prototype.getPositionAtE = function(E) {
+        //Part I: gather Orbital Elements
+        return this.getPosition();
+    }
+
+   var demoBox = $('#demo');
+    var demoTabs = $('#tabs');
+    demoTabs.tabs({
+        collapsible:true
+    });
+
+    $('#slider-star-mass').slider({
+         value:1
+        ,min:0.0
+        ,max:10.1
+        ,step:0.1
+        ,slide: function(event,ui) {
+            $(this).parent().find("input").val(ui.value + " Sols");
+        }
+        ,stop: function(event, ui) {
+            elementChange()
+        }
+    });
+    $('#slider-planet-mass').slider({
+         value:1
+        ,min:0.0
+        ,max:10.1
+        ,step:0.1
+        ,slide: function(event,ui) {
+            $(this).parent().find("input").val(ui.value + " Earths");
+        }
+        ,stop: function(event, ui) {
+            elementChange()
+        }
+    });
+    $('#slider-moon-mass').slider({
+         value:1
+        ,min:0.0
+        ,max:10.1
+        ,step:0.1
+        ,slide: function(event,ui) {
+            $(this).parent().find("input").val(ui.value + " Moons");
+        }
+        ,stop: function(event, ui) {
+            elementChange()
+        }
+    });
+    $('#star-mass').val( function() {
+        return ($('#slider-star-mass').slider("value")  + " Sols");
+    });
+    $('#planet-mass').val( function() {
+        return ($('#slider-planet-mass').slider("value")  + " Earths");
+    });
+    $('#moon-mass').val( function() {
+        return ($('#slider-moon-mass').slider("value")  + " Moons");
+    });
+    $('#slider-planet-a').slider({
+         value:1
+        ,min:0.0
+        ,max:10.1
+        ,step:0.1
+        ,slide: function(event,ui) {
+            $(this).parent().find("input").val(ui.value + " AU");
+        }
+        ,stop: function(event, ui) {
+            elementChange()
+        }
+    });
+    $('#slider-moon-a').slider({
+         value:300000
+        ,min: 0.0
+        ,max: 3030000
+        ,step:30000
+        ,slide: function(event,ui) {
+            $(this).parent().find("input").val(ui.value + " km");
+        }
+        ,stop: function(event, ui) {
+            elementChange()
+        }
+    });
+    $('#planet-a').val( function() {
+        return ($('#slider-planet-a').slider("value")  + " AU");
+    });
+    $('#moon-a').val( function() {
+        return ($('#slider-moon-a').slider("value")  + " km");
+    });
+    $("[id^=slider][id$=ecc]").slider({
+         value:0.0
+        ,min:0.0
+        ,max:1.1
+        ,step:0.1
+        ,slide: function(event,ui) {
+            $(this).parent().find("input").val(ui.value + "");
+        }
+        ,stop: function(event, ui) {
+            elementChange()
+        }
+    });
+    $('input[id$=ecc]').val( function() {
+        return ($(this).parent().find("[id^=slider][id$=ecc]").slider("value")  + "");
+    });
+    $("[id^=slider][id$=mAnomaly]").slider({
+         value:0
+        ,min:0.0
+        ,max:360
+        ,step:5
+        ,slide: function(event,ui) {
+            $(this).parent().find("input").val(ui.value + " \xB0");
+        }
+        ,stop: function(event, ui) {
+            elementChange()
+        }
+    });
+    $('input[id$=mAnomaly]').val( function() {
+        return ($(this).parent().find("[id^=slider][id$=mAnomaly]").slider("value")  + " \xB0");
+    });
+    $("[id^=slider][id$=rotI]").slider({
+         value:0
+        ,min:-90
+        ,max:90
+        ,step:5
+        ,slide: function(event,ui) {
+            $(this).parent().find("input").val(ui.value + " \xB0");
+        }
+        ,stop: function(event, ui) {
+            elementChange()
+        }
+    });
+    $('input[id$=rotI]').val( function() {
+        return ($(this).parent().find("[id^=slider][id$=rotI]").slider("value")  + " \xB0");
+    });
+    $("[id^=slider][id$=rotW]").slider({
+         value:0
+        ,min:0.0
+        ,max:360
+        ,step:5
+        ,slide: function(event,ui) {
+            $(this).parent().find("input").val(ui.value + " \xB0");
+        }
+        ,stop: function(event, ui) {
+            elementChange()
+        }
+    });
+    $('input[id$=rotW]').val( function() {
+        return ($(this).parent().find("[id^=slider][id$=rotW]").slider("value")  + " \xB0");
+    });
+    $("[id^=slider][id$=rotOmeg]").slider({
+         value:0
+        ,min:0.0
+        ,max:360
+        ,step:5
+        ,slide: function(event,ui) {
+            $(this).parent().find("input").val(ui.value + "\xB0");
+        }
+        ,stop: function(event, ui) {
+            elementChange()
+        }
+    });
+    $('input[id$=rotOmeg]').val( function() {
+        return ($(this).parent().find("[id^=slider][id$=rotOmeg]").slider("value")  + " \xB0");
+    });
+
+
+    elementChange();
 });
